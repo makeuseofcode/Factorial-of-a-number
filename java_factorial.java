0bedf925467577fee76def4f1091b64de3ba27e0
Sai Ashish <saiashishkonchada@gmail.com>
2022-10-11 12:19:40 +0530

    Create java_factorial.java

---
+++ java_factorial.java
@@ -0,0 +1,15 @@
+class Main {
+	static int fact(int n)
+	{
+		if (n == 0)
+			return 1;
+
+		return n * fact(n - 1);
+	}
+
+	public static void main(String[] args)
+	{
+		int num = 5;
+		System.out.println("Factorial of " + num + " is " + fact(num));
+	}
+}
